97f187ef6ffcbcaf069c641b756d4ecb7bb52e3f
Konrad Ferdinand Heimel <konrad.heimel@gmail.com>
2019-07-10 00:16:59 +0200

    Week 2 - Exercise 2 - Fibonacci Last Digit

--- src/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/FibonacciLastDigit.kt
+++ src/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/FibonacciLastDigit.kt
@@ -11,14 +11,33 @@ fun getFibonacciLastDigitNaive(n: Int): Int {
         val tmpPrevious = previous
         previous = current
         current += tmpPrevious
+        current %= 10
     }
 
     return current % 10
 }
 
+fun getFibonacciLastDigit(n: Int): Int {
+    var nshort = n % 60
+    if (nshort <= 1)
+        return nshort
+    var previous = 0
+    var current = 1
+
+    for (i in 0 until nshort - 1) {
+        val tmpPrevious = previous
+        previous = current
+        current += tmpPrevious
+        current %= 10
+    }
+
+    return current
+}
+
 fun main(args: Array<String>) {
     val scanner = Scanner(System.`in`)
     val n = scanner.nextInt()
-    val c = getFibonacciLastDigitNaive(n)
+
+    val c = getFibonacciLastDigit(n)
     println(c)
 }
