96cfe5d148d72c3ff9a02f3e04a0140b3567766c
Konrad Ferdinand Heimel <konrad.heimel@gmail.com>
2019-07-10 00:04:09 +0200

    Week 2 - Exercise 1 - Fibonacci

--- src/week2_algorithmic_warmup/1_fibonacci_number/Fibonacci.kt
+++ src/week2_algorithmic_warmup/1_fibonacci_number/Fibonacci.kt
@@ -1,12 +1,24 @@
 import java.util.*
 
-fun calcFib(n: Long): Long {
-    return if (n <= 1) n else calcFib(n - 1) + calcFib(n - 2)
+fun calcFibSlow(n: Long): Long {
+    return if (n <= 1) n else calcFibSlow(n - 1) + calcFibSlow(n - 2)
 }
-
+fun calcFib(n: Long): Long {
+    if (n <= 1){
+        return n
+    }
+    var nMinus1:Long = 0
+    var nMinus2:Long = 1
+    var current:Long = 0
+    for (i in 1..n){
+        current = nMinus1 + nMinus2
+        nMinus2 = nMinus1;
+        nMinus1 = current;
+    }
+    return current;
+}
 fun main(args: Array<String>) {
     val scanner = Scanner(System.`in`)
     val n = scanner.nextLong()
-
     println(calcFib(n))
 }
